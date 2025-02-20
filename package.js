fe08fa573fd1683b0e61111b3c965295043a552f
Opoku Akwasi Gibson <gibsonopokuwalkerjnr@gmail.com>
2025-02-19 21:24:16 -0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,22 @@
+// client/package.json
+// Title: Client Package Configuration
+// Purpose: Lists dependencies and scripts for the React Native app
+
+{
+  "name": "InstaAutoPromo",
+  "version": "1.0.0",
+  "main": "node_modules/expo/AppEntry.js",
+  "scripts": {
+    "start": "expo start",
+    "android": "expo start --android",
+    "ios": "expo start --ios"
+  },
+  "dependencies": {
+    "expo": "~51.0.0",
+    "react": "18.2.0",
+    "react-native": "0.74.5"
+  },
+  "devDependencies": {
+    "@babel/core": "^7.20.0"
+  }
+}
