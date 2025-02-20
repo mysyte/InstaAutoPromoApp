70d589ba2c7f0896b4d8b9118c262ffab202ff34
Opoku Akwasi Gibson <gibsonopokuwalkerjnr@gmail.com>
2025-02-19 21:23:34 -0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+// client/App.js
+// Title: App Entry Point
+// Purpose: Renders the main Dashboard screen
+
+import React from 'react';
+import Dashboard from './src/screens/Dashboard';
+
+const App = () => {
+  return <Dashboard />;
+};
+
+export default App;
