6670cd8aa2edd4aa3b3f187262adf502dd093ce9
Opoku Akwasi Gibson <gibsonopokuwalkerjnr@gmail.com>
2025-02-19 21:13:11 -0700

    Create Dashboard.js

---
+++ Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+const Dashboard = () => {
+  return (
+    <View>
+      <Text>Next Post: 6 PM</Text>
+      <Button title="Schedule New Post" onPress={() => {}} />
+    </View>
+  );
+};
+
+export default Dashboard;
